5a366b190d61548885193fe3f22bb4c596f1a069
holmista <tornikebuchukuri@redberry.ge>
2022-09-07 23:56:13 +0400

    feat: sign up fields validation

---
+++ app/Http/Requests/StoreSignupRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSignupRequest extends FormRequest
+{
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function rules()
+	{
+		return [
+			'name'           => ['required', 'min:3', 'unique:users,name'],
+			'email'          => ['required', 'email', 'unique:users,email'],
+			'password'       => ['required', 'min:3'],
+			'repeat_password'=> ['same:password'],
+		];
+	}
+}
