7e6f35bf1dd5666446ccbe02cea450472ca05be9
holmista <tornikebuchukuri@redberry.ge>
2022-09-13 23:02:08 +0400

    fix: add border under panel

--- resources/views/components/stats-worldCountry.blade.php
+++ resources/views/components/stats-worldCountry.blade.php
@@ -1,4 +1,4 @@
-<div class="text-[#010414] font-bold text-base flex gap-16 mt-5">
+<div class="text-[#010414] font-bold text-base flex gap-16 mt-5 border-b-2 border-[#F6F6F7]">
     <p class="py-5 border-b-4 border-[#010414]">Worldwide</p>
     <p class="py-5">By country</p>
 </div>
