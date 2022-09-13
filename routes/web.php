038bca652da1f3ac48430080601510de65e255c3
holmista <tornikebuchukuri@redberry.ge>
2022-09-13 14:03:36 +0400

    refactor: routes

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,22 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::middleware(['guest'])->group(function () {
+	Route::controller(AuthController::class)->group(function () {
+		Route::post('/signup', 'signup')->name('auth.signup')->name('auth.signup');
+		Route::get('/account-confirmed', 'accountConfirmed')->middleware('requestedVerification')->name('auth.view_account_confirmed');
+		Route::get('/email/verify', 'emailSent')->middleware('requestedVerification')->name('verification.notice');
+		Route::post('/signin', 'signin')->name('auth.signin');
+		Route::post('/forgot-password', 'forgotPassword')->name('auth.forgot_password');
+		Route::get('/email/verify/{id}/{hash}', 'verifyEmail')->name('verification.verify');
+		Route::get('/password/verify/{id}/{token}', 'showResetPassword')->name('password.request');
+		Route::patch('/reset-password', 'resetPassword')->name('password.reset');
+	});
 	Route::view('/signup', 'auth.signup')->name('auth.view_signup');
-	Route::post('/signup', [AuthController::class, 'signup'])->name('auth.signup');
 	Route::view('/signin', 'auth.signin')->name('auth.view_signin');
-	Route::get('/account-confirmed', [AuthController::class, 'accountConfirmed'])->middleware('requestedVerification')->name('auth.view_account_confirmed');
-	Route::get('/email/verify', [AuthController::class, 'emailSent'])->middleware('requestedVerification')->name('verification.notice');
-	Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
-	Route::post('/signin', [AuthController::class, 'signin']);
 	Route::view('/forgot-password', 'auth.forgot-password')->name('password.forgot');
-	Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-	Route::get('/password/verify/{id}/{token}', [AuthController::class, 'showResetPassword'])->name('password.request');
-	Route::patch('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
 });
 
 Route::middleware(['auth'])->group(function () {
 	Route::view('/', 'home')->name('home.index');
-	Route::get('/signout', [AuthController::class, 'signout']);
+	Route::get('/signout', [AuthController::class, 'signout'])->name('auth.signout');
 });
