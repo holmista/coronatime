dfa5704ec85f5b28a1ed3b10874bd9db9d2e9acd
holmista <tornikebuchukuri@redberry.ge>
2022-09-13 19:51:44 +0400

    feat: country and worldwide panel

---
+++ resources/views/components/stats-worldCountry.blade.php
@@ -0,0 +1,4 @@
+<div class="text-[#010414] font-bold text-base flex gap-16 mt-5">
+    <p class="py-5 border-b-4 border-[#010414]">Worldwide</p>
+    <p class="py-5">By country</p>
+</div>
--- resources/views/stats/landing.blade.php
+++ resources/views/stats/landing.blade.php
@@ -1,3 +1,11 @@
 <x-generic-layout>
-    <x-auth-navbar />
+    <x-stats-navbar />
+    <div class="w-full flex justify-center">
+        <div class="w-11/12">
+            <h1 class="text-2xl text-[#010414] font-extrabold mt-10">
+                Worldwide Statistics
+            </h1>
+            <x-stats-worldCountry />
+        </div>
+    </div>
 </x-generic-layout>
