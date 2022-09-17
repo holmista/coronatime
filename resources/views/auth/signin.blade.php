cd3fd95c8707017db7ab777896a6b2932262b4b4
holmista <tornikebuchukuri@redberry.ge>
2022-09-17 18:24:53 +0400

    feat: translate signin and signup pages

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -4,14 +4,16 @@
             <div class="w-11/12">
                 <section>
                     <img src="/storage/logo.png" alt="">
-                    <h1 class="font-black text-[#010414] text-2xl mt-14">Welcome back</h1>
-                    <h3 class="text-lg text-[#808189]">Welcome back! Please enter your details</h3>
+                    <h1 class="font-black text-[#010414] text-2xl mt-14">{{ __('texts.welcome_back') }}</h1>
+                    <h3 class="text-lg text-[#808189]">{{ __('texts.welcome_back_please_enter_your_details') }}</h3>
                 </section>
                 <form method="POST" action="/signin" class="mt-6">
                     @csrf
                     <div class="space-y-5">
-                        <x-input-field name="username" type="text" placeholder="Enter unique username or email" />
-                        <x-input-field name="password" type="password" placeholder="Fill in password" />
+                        <x-input-field name="{{ __('texts.username') }}" type="text"
+                            placeholder="{{ __('texts.enter_unique_username_or_email') }}" />
+                        <x-input-field name="{{ __('texts.password') }}" type="password"
+                            placeholder="{{ __('texts.fill_in_password') }}" />
                         <div class="mt-6 flex justify-between">
                             <div class="relative flex items-start">
                                 <div class="flex h-5 items-center">
@@ -19,19 +21,19 @@
                                         class="h-4 w-4 rounded border-gray-300 text-[#249E2C] focus:ring-white">
                                 </div>
                                 <div class="ml-3 text-sm">
-                                    <label for="remember" class="font-bold text-sm text-[#010414]">Remember this
-                                        device</label>
+                                    <label for="remember"
+                                        class="font-bold text-sm text-[#010414]">{{ __('texts.remember_this_device') }}</label>
                                 </div>
                             </div>
                             <a href="/forgot-password">
-                                <p class="text-[#2029F3] text-sm">Forgot password?</p>
+                                <p class="text-[#2029F3] text-sm">{{ __('texts.forgot_password') }}</p>
                             </a>
                         </div>
                         <x-auth-button text="SIGN IN" type="submit" />
                         <p class="text-[#808189] ">
-                            Don't have and account?
+                            {{ __('texts.dont_have_an_account') }}
                             <a href="/signup" class="hover: cursor-pointer">
-                                <strong class="text-[#010414]">Sign up for free</strong>
+                                <strong class="text-[#010414]">{{ __('texts.sign_up_for_free') }}</strong>
                             </a>
                         </p>
                     </div>
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -4,23 +4,27 @@
             <div class="w-11/12">
                 <section>
                     <img src="/storage/logo.png" alt="">
-                    <h1 class="font-black text-[#010414] text-2xl mt-14">Welcome to Coronatime</h1>
-                    <h3 class="text-lg text-[#808189]">Please enter required info to sign up</h3>
+                    <h1 class="font-black text-[#010414] text-2xl mt-14">{{ __('texts.welcome_to_coronatime') }}</h1>
+                    <h3 class="text-lg text-[#808189]">{{ __('texts.please_enter_required_info_to_sign_up') }}</h3>
                 </section>
                 <form method="POST" action="/signup" class="mt-6">
                     @csrf
                     <div class="space-y-5">
-                        <x-input-field name="username" type="text" placeholder="Enter unique username"
-                            moreInfo="Username should be unique, min 3 symbols" />
-                        <x-input-field name="email" type="email" placeholder="Enter your email" />
-                        <x-input-field name="password" type="password" placeholder="Fill in password" />
-                        <x-input-field name="repeat_password" type="password" placeholder="Repeat password" />
+                        <x-input-field name="{{ __('texts.username') }}" type="text"
+                            placeholder="{{ __('texts.enter_unique_username') }}"
+                            moreInfo="{{ __('texts.username_should_be_unique_min_3_symbols') }}" />
+                        <x-input-field name="{{ __('texts.email') }}" type="email"
+                            placeholder="{{ __('texts.enter_your_email') }}" />
+                        <x-input-field name="{{ __('texts.password') }}" type="password"
+                            placeholder="{{ __('texts.fill_in_password') }}" />
+                        <x-input-field name="{{ __('texts.repeat_password') }}" type="password"
+                            placeholder="{{ __('texts.repeat_password') }}" />
 
-                        <x-auth-button text="SIGN UP" type="submit" />
+                        <x-auth-button text="{{ __('texts.sign_up') }}" type="submit" />
                         <p class="text-[#808189] ">
-                            Already have an account?
+                            {{ __('texts.already_have_an_account') }}
                             <a href="/signin" class="hover: cursor-pointer">
-                                <strong class="text-[#010414]">Log in</strong>
+                                <strong class="text-[#010414]">{{ __('texts.log_in') }}</strong>
                             </a>
                         </p>
                     </div>
